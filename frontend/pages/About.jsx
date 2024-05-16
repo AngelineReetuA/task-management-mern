617f888b6550f5d8d5750793cfba49043aaffd3e
Angeline Reetu A <angelinereetu@gmail.com>
2024-05-16 14:04:11 +0530

    Onboarding page designed

---
+++ frontend/pages/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/styles.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="container p-5">
+        <h4 className="sourceCodePro fw-bold">Spread</h4>
+        <br />
+        <p>
+          Ditch the siloed spreadsheets and fragmented communication! <br />
+          <br />
+          <p>
+            Spread brings streamlined project management to your fingertips. Our
+            intuitive application empowers you to organize tasks, collaborate
+            seamlessly, and keep everyone on the same page. Spread acts as the
+            central hub for your projects, fostering clarity, boosting
+            efficiency, and ensuring deadlines are met with a flourish.
+          </p>
+          <p>Organize your work life with Spread.</p>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import { HomeHeader } from "../components/HomeHeader";
+import { Onboarding } from "../pages/Onboarding";
+import About from "../pages/About";
+import Help from "../pages/Help";
+
+function App() {
   return (
     <>
-      Initialized
+      <div>
+        <Router>
+          <HomeHeader />
+          <Routes>
+            <Route exact path="/" element={<Onboarding />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/help" element={<Help />} />
+          </Routes>
+        </Router>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
