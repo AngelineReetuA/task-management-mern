38e3cf380ebb72a68df73ce865fe81dedcaabca1
Angeline Reetu A <angelinereetu@gmail.com>
2024-06-03 17:56:19 +0530

    removed firebase from backend

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
-const {
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-} = require("firebase/auth");
-const { auth } = require("./config/firebase.config");
 
 app.use(express.json());
 
-app.post("/register", async (req, res) => {
-  const user = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  try {
-    await createUserWithEmailAndPassword(auth, user.email, user.password).then(
-      async (userCred) => {
-        const user = userCred.user;
-        await sendEmailVerification(user);
-      }
-    );
-    return res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.listen(port, () => {
   console.log(`TSM listening on port ${port}`);
