724b42903cabf807e3e0dbe602e184d3fa2d617c
Angeline Reetu A <angelinereetu@gmail.com>
2024-06-04 18:31:12 +0530

    added team creating / joining page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import { HomeHeader } from "../components/HomeHeader";
 import { Onboarding } from "../pages/Onboarding";
 import About from "../pages/About";
 import Help from "../pages/Help";
+import { TeamPage } from "../pages/TeamPage";
 
 function App() {
   return (
     <>
       <div>
         <Router>
-          <HomeHeader />
           <Routes>
             <Route exact path="/" element={<Onboarding />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/help" element={<Help />} />
+            <Route exact path="/:mail" element={<TeamPage />} />
           </Routes>
         </Router>
       </div>
